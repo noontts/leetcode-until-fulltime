9187368ae45d52b3a0144761ce3018ca437d4151
thitisan.k <noonthitisan@gmail.com>
2025-04-02 11:25:11 +0700

    test: added testcase.

--- test/linked-list/merge_two_sorted_lists_test.go
+++ test/linked-list/merge_two_sorted_lists_test.go
@@ -14,13 +14,17 @@ func TestContainsDuplicate(t *testing.T) {
 		list1  *utils.ListNode
 		list2  *utils.ListNode
 		expect *utils.ListNode
-	}{}
+	}{
+		{utils.NewList([]int{1, 2, 4}), utils.NewList([]int{1, 3, 4}), utils.NewList([]int{1, 1, 2, 3, 4, 4})},
+		{utils.NewList([]int{}), utils.NewList([]int{}), utils.NewList([]int{})},
+		{utils.NewList([]int{}), utils.NewList([]int{0}), utils.NewList([]int{0})},
+	}
 
 	for i, tc := range testCase {
 		t.Run("Merge Two Sorted Lists case: "+strconv.Itoa(i), func(t *testing.T) {
 			t.Parallel()
 			result := linkedlist.MergeTwoLists(tc.list1, tc.list2)
-			assert.Equal(t, tc.expect, result, "they should be equal")
+			assert.True(t, utils.CompareLists(result, tc.expect), "List value must equal")
 		})
 	}
 }
